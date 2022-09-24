c1d372e1bd8dc10eb2afb33deb633ae0009e8b82
EluciusFTW <guysbuss@gmail.com>
2022-09-24 11:03:32 +0200

    Made styles mutable

--- src/spectre-fs/Output.fs
+++ src/spectre-fs/Output.fs
@@ -1,11 +1,11 @@
 module SpectreFs.Output
 open Spectre.Console
 
-let primaryStyle = "green"
-let warningStyle = "red"
+let mutable emphasizeStyle = "green"
+let mutable warningStyle = "red"
 
 let markup style content = $"[{style}]{content}[/]"
-let emphasize content = markup primaryStyle content
+let emphasize content = markup emphasizeStyle content
 let warn content = markup warningStyle content
 
 let printMarkedUp content = AnsiConsole.Markup $"{content}{System.Environment.NewLine}"
