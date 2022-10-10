4e1fced02f85e4ee29b7f8c854e55944f4988fec
EluciusFTW <guysbuss@gmail.com>
2022-10-10 23:43:41 +0200

    Added example and doc subcommands for rule

--- src/sample-cli/commands/Rule.fs
+++ src/sample-cli/commands/Rule.fs
@@ -3,17 +3,42 @@
 open Spectre.Console.Cli
 open SpectreFs.Rule
 open SpectreFs.Output
+open SpectreFs.Sample
 
-type RuleExampleSettings()  =
+type RuleSettings()  =
     inherit CommandSettings()
 
 type RuleExample() =
-    inherit Command<RuleExampleSettings>()
-    interface ICommandLimiter<RuleExampleSettings>
+    inherit Command<RuleSettings>()
+    interface ICommandLimiter<RuleSettings>
 
     override _.Execute(_context, _settings) =
         alignedRule Left  $"""{emphasize "Hello"}"""
         rule "Fellow"
         alignedRule Right $"""{warn "Developer"}"""
         emptyRule
+        0
+
+type RuleDocumentation() =
+    inherit Command<RuleSettings>()
+    interface ICommandLimiter<RuleSettings>
+
+    override _.Execute(_context, _settings) =
+        Theme.setDocumentationStyle
+        
+        printfn ""
+        alignedRule Left (emphasize "Rule module")
+        ManyMarkedUp [
+            CO [S "This module provides functionality from the "; E "rule widget"; S " of Spectre.Console"]
+            NewLine
+            S "The rule can be used by the rule function:"
+            BI [ 
+                E "rule: string -> unit"
+            ]
+            C $"""{standard "This rule will use the default style, which can be changed by modifying "}{emphasize "Rule.defaultAlignment"}."""
+            S "It is set to 'Center' by default. Other rules can be used without changing the default by passing in the alignment as an argument to: "
+            BI [ 
+                E "alignedRule: Alignment -> string -> unit"
+            ]
+        ] |> toConsole
         0
