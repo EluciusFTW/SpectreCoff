719f5f36e1dbf0a7afe3f487809f6f8dbf5cb1fc
EluciusFTW <guysbuss@gmail.com>
2022-10-13 23:59:52 +0200

    Fixed typo in the name

--- src/spectrecoff/Table.fs
+++ src/spectrecoff/Table.fs
@@ -1,6 +1,31 @@
 module SpectreCoff.Table
 open Spectre.Console
 
+type SizingBehaviour =
+    | Expand
+    | Collapse
+
+type Alignment =
+    | Left
+    | Center
+    | Right
+
+type TableLayout = 
+    {
+        Border: TableBorder;
+        Sizing: SizingBehaviour;
+        HideHeaders: bool;
+        Alignment: Alignment
+    }
+
+let (defaultTableLayout: TableLayout) = 
+    {
+        Border = TableBorder.Rounded
+        Sizing = Expand
+        Alignment = Left
+        HideHeaders = false
+    }
+
 let toRenderableRow (items: Rendering.IRenderable list) =
     TableRow(items)
 
@@ -13,14 +38,28 @@ let toColumn (value: string) =
 let toColumns values =
     values |> List.map(fun v -> toColumn (v.ToString()))
 
-let table (columns: TableColumn list) (rows: TableRow list) =
+let table (layout: TableLayout) (columns: TableColumn list) (rows: TableRow list) =
     let table = new Table()
+
+    match layout.Alignment with
+    | Left -> table.LeftAligned() |> ignore
+    | Right -> table.RightAligned() |> ignore
+    | Center -> table.Centered() |> ignore
+
+    match layout.Sizing with
+    | Expand -> table.Expand <- true
+    | Collapse -> table.Collapse() |> ignore
+
+    match layout.HideHeaders with
+    | true -> table.HideHeaders() |> ignore
+    | false -> () |> ignore
+    
     columns |> List.iter (fun column -> table.AddColumn(column) |> ignore)
     rows |> List.iter (fun row -> table.AddRow(row) |> ignore)
     table
 
 let stable columns rows =
-    table (toColumns columns) (rows |> List.map toStringRow)
+    table defaultTableLayout (toColumns columns) (rows |> List.map toStringRow)
 
 let print table = AnsiConsole.Write (table: Table)
-let printcr columns rows = print (table rows columns)
+let printcr columns rows = print (table defaultTableLayout rows columns)
