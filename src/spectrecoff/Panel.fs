ac3e54f28f0d6d1e63ede07599eb79d98fccbe9c
EluciusFTW <guysbuss@gmail.com>
2022-10-19 00:23:53 +0200

    Added panel module

---
+++ src/spectrecoff/Panel.fs
@@ -0,0 +1,35 @@
+module SpectreCoff.Panel
+
+open Spectre.Console
+open SpectreCoff.Layout
+
+type PanelLayout = 
+    {  Border: BoxBorder;
+       Sizing: SizingBehaviour;
+       Padding: Padding }
+
+let defaultPanelLayout: PanelLayout = 
+    {  Border = BoxBorder.Heavy
+       Sizing = Collapse
+       Padding = AllEqual 2 }
+
+let customPanel (layout: PanelLayout) (header: string) (content: string) =
+    let panel = new Panel(content)
+    panel.Header <- new PanelHeader(header)
+
+    match layout.Sizing with
+    | Expand -> panel.Expand <- true
+    | Collapse -> panel.Collapse() |> ignore
+    
+    match layout.Padding with 
+    | AllEqual value -> panel.Padding <- new Spectre.Console.Padding(value)
+    | HorizontalVertical (hor,vert) -> panel.Padding <- new Spectre.Console.Padding(hor, vert)
+    | TopRightBottomLeft (l,t,r,b) -> panel.Padding <- new Spectre.Console.Padding(l, t, r, b)
+    
+    panel
+
+let panel =
+   customPanel defaultPanelLayout
+
+let toConsole (panel: Panel) =
+    panel |> AnsiConsole.Write
