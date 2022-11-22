1dc3d21f826381c44e6f2b51ef7dc8182170aea9
EluciusFTW <guysbuss@gmail.com>
2022-11-22 19:44:29 +0100

    Handle renderable payloads correctly when writing

--- src/spectrecoff-cli/commands/Output.fs
+++ src/spectrecoff-cli/commands/Output.fs
@@ -81,7 +81,7 @@ type OutputExample() =
                 P "several"
                 E "items"
             ]
-            NewLine
+            SpectreCoff.Rule.rule "Links  and Emojis"
             CO [
                 C "You can easily render clickable links:"
                 Link "https://www.spectreconsole.net/markup"
