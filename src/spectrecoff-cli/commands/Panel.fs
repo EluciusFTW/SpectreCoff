433a900bc882d63eacc7c260b165be74529e5e3d
EluciusFTW <guysbuss@gmail.com>
2022-12-03 00:32:15 +0100

    Added documentation for panel module

--- src/spectrecoff-cli/commands/Panel.fs
+++ src/spectrecoff-cli/commands/Panel.fs
@@ -39,4 +39,42 @@ type PanelDocumentation() =
     
     override _.Execute(_context, _) = 
         Cli.Theme.setDocumentationStyle
+
+        NewLine |> toConsole
+        pumped "Panel module"
+        |> alignedRule Left
+        |> toConsole
+
+        Many [
+            CO [
+                C "This module provides functionality from the panel widget of Spectre.Console ("
+                Link "https://spectreconsole.net/widgets/panel"
+                C ")"
+            ]
+            NL
+            C "The panel can be used by the panel function:"
+            BI [P "panel: (header: string) -> (content: OutputPayload) -> OutputPayload"]
+            NL
+            CO [
+                C "While the content can be an arbitrary payload" 
+                E "(known bug: Many does not work inside panel, fix on the way)"
+                C "the header needs to be a string."
+                C "Spectre still will render markup here, though, so all"; 
+                P "stringifyable payloads"; 
+                C "can be used here when mapped to string using the extension"; 
+                P "payload.toMarkedUpString."
+            ]
+            NL
+            CO [
+                C "This panel will use the"; 
+                P "Panel.defaultPanelLayout,"; 
+                C "initialized to heavy border, collapsed expansion behaviour and a padding of 2 units on all sides."
+            ]
+            NL
+            C "In order to produce a panel with other layout, the function"
+            BI [P "customPanel: PanelLayout -> string -> OutputPayload -> Renderable"]
+            NL
+            C "can be used. Alternatively, the default can also be modified."
+            NL
+        ] |> toConsole
         0
--- src/spectrecoff-cli/commands/Rule.fs
+++ src/spectrecoff-cli/commands/Rule.fs
@@ -47,14 +47,14 @@ type RuleDocumentation() =
             NewLine
             C "The rule can be used by the rule function:"
             BI [ 
-                P "rule: string -> Rule"
+                P "rule: string -> OutputPayload"
             ]
             NL
             CO [C "This rule will use the"; P "Rule.defaultAlignment,"; C "which is set to"; P "Center"; C "but can be modified."]
             NL
             C "Other rules can be used without changing the default by passing in the alignment as an argument to: "
             BI [ 
-                P "alignedRule: Alignment -> string -> Renderable"
+                P "alignedRule: Alignment -> string -> OutputPayload"
             ]
             NL
             CO [C "The rule can be printed to the console with the"; P "toConsole"; C "function."]
