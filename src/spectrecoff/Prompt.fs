7d3d091c4b8011934be034d902ffed2359ddd1f8
EluciusFTW <guysbuss@gmail.com>
2022-12-12 00:55:05 +0100

    Extended prompt

--- src/spectrecoff/Prompt.fs
+++ src/spectrecoff/Prompt.fs
@@ -3,17 +3,56 @@ module SpectreCoff.Prompt
 
 open Spectre.Console 
 
-let selectionPrompt question choices = 
-    let prompt = SelectionPrompt()
-    prompt.AddChoices (choices |> Seq.toArray) |> ignore
-    prompt.Title <- question
-    prompt
+type PromptOptions = 
+    { Secret: bool
+      Optional: bool }
 
-let prompt prompter =
+let defaultOptions = { Secret = false; Optional = false}
+
+[<RequireQualifiedAccess>]
+module Prompts = 
+    let selectionPrompt question choices = 
+        let prompt = SelectionPrompt()
+        prompt.AddChoices (choices |> Seq.toArray) |> ignore
+        prompt.Title <- question
+        prompt
+
+    let multiSelectionPrompt question choices = 
+        let prompt = MultiSelectionPrompt()
+        prompt.AddChoices (choices |> Seq.toArray) |> ignore
+        prompt.Title <- question
+        prompt
+
+    let textPrompt<'T> question (options: PromptOptions) = 
+        let prompt = TextPrompt<'T> question
+        prompt.IsSecret <- options.Secret
+        prompt.AllowEmpty <- options.Optional
+        prompt
+
+    let textPromptWithDefault<'T> question (answer: 'T) (options: PromptOptions) =
+        let prompt = textPrompt<'T> question options
+        prompt.DefaultValue answer
+
+let private prompt prompter =
     AnsiConsole.Prompt prompter;
 
-let promptChoices title choices = 
-    prompt (selectionPrompt title choices)
+let chooseFrom choices question = 
+    prompt (Prompts.selectionPrompt question choices)
 
-let confirm message = 
-    AnsiConsole.Confirm message
+let chooseMultipleFrom choices question = 
+    prompt (Prompts.multiSelectionPrompt question choices)
+
+let ask<'T> question = 
+    prompt (Prompts.textPrompt<'T> question defaultOptions)
+
+let askWith<'T> options question = 
+    prompt (Prompts.textPrompt<'T> question options)
+
+let askSuggesting<'T> answer question = 
+    prompt (Prompts.textPromptWithDefault<'T> question answer defaultOptions)
+
+let askWithSuggesting<'T> options answer question = 
+    prompt (Prompts.textPromptWithDefault<'T> question answer options)
+
+let confirm question = 
+    AnsiConsole.Confirm question
