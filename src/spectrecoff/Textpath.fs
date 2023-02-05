2b6b013f8317e8cbaaf24546e1e79a3ece996183
EluciusFTW <guysbuss@gmail.com>
2023-02-05 14:56:45 +0100

    Added styles and alignment to path

--- src/spectrecoff/Textpath.fs
+++ src/spectrecoff/Textpath.fs
@@ -5,11 +5,31 @@ open Spectre.Console
 open SpectreCoff.Layout
 open SpectreCoff.Output
 
-type TextPathOptions = 
-    { Border: TableBorder;
-      Sizing: SizingBehaviour;
-      HideHeaders: bool;
-      Alignment: Alignment }
+let mutable stemColor = calmColor
+let mutable rootColor = calmColor
+let mutable separatorColor = pumpedColor
+let mutable leafColor = edgyColor
 
-let path content = 
-    TextPath content
+let mutable defaultAlignment = Left
+
+let private applyColors (path: TextPath) =
+    path.LeafColor leafColor |> ignore
+    path.SeparatorColor separatorColor |> ignore
+    path.StemColor stemColor |> ignore
+    path.RootColor rootColor
+
+let alignedPath alignment value =
+    let path = TextPath value
+
+    match alignment with
+    | Left -> path.LeftJustified() |> ignore
+    | Center -> path.Centered() |> ignore
+    | Right -> path.RightJustified() |> ignore
+
+    path 
+    |> applyColors 
+    :> Rendering.IRenderable
+    |> Renderable
+
+let path = 
+    alignedPath defaultAlignment
