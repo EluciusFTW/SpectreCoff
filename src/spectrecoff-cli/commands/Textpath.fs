157479e806d2505bb019a6725c7caf1c3a9a4650
EluciusFTW <guysbuss@gmail.com>
2023-02-05 14:57:01 +0100

    Added example and documentation to textpath

--- src/spectrecoff-cli/commands/Textpath.fs
+++ src/spectrecoff-cli/commands/Textpath.fs
@@ -12,6 +12,22 @@ type TextpathExample() =
     interface ICommandLimiter<TextpathSettings>
 
     override _.Execute(_context, _) =
+        
+        let examples = [
+            "C:\\Temp\\local\\data.json"
+            "C:/temp/local"
+            "C:/This/Path/Is/Too/Long/To/Fit/In/The/Area/This/Path/Is/Too/Long/To/Fit/In/The/Area/This/Path/Is/Too/Long/To/Fit/In/The/Area.txt" 
+        ]
+
+        alignedRule Left "Formatting examples" |> toConsole
+        examples 
+        |> List.map path 
+        |> List.iter toConsole
+
+        alignedRule Left "Alignment examples" |> toConsole
+        [Left; Center; Right]
+        |> List.map (fun alignment -> alignedPath alignment examples.Head)
+        |> List.iter toConsole
         0
 
 type TextpathDocumentation() =
@@ -19,6 +35,36 @@ type TextpathDocumentation() =
     interface ICommandLimiter<TextpathSettings>
 
     override _.Execute(_context, _) =
+
         Cli.Theme.setDocumentationStyle
-        Edgy "Sorry, this documentation is not available yet." |> toConsole    
+
+        NewLine |> toConsole
+        pumped "Textpath module"
+        |> alignedRule Left
+        |> toConsole
+        
+        Many [
+            CO [
+                C "This module provides functionality from the text path widget of Spectre.Console ("
+                Link "https://spectreconsole.net/widgets/text-path"
+                C ")"
+            ]
+            NL
+            C "The text path can be used by the path function:"
+            BI [P "path: (value: string) -> OutputPayload"]
+            NL
+            CO [C "This path will use the"; P "Textpath.defaultAlignment,"; C "which is set to"; P "Left"; C "but can be modified."]
+            C "Other alignments can be used without changing the default by passing in the alignment as an argument to: "
+            BI [ 
+                P "alignedPath: Alignment -> string -> OutputPayload"
+            ]
+            NL
+            C "The path is rendered using four styles, which are mutable. The defaults are linked to the convenience style colors:"
+            BI [
+                CO [P "rootColor"; C  "(default: calmColor)"]
+                CO [P "stemColor"; C  "(default: calmColor)"]
+                CO [P "separatorColor"; C  "(default: pumperColor)"]
+                CO [P "leafColor"; C  "(default: edgyColor)"]
+            ]
+        ] |> toConsole
         0
