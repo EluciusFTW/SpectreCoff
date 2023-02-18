6f60a07490054f8db4c493ae5e5e35dfd799ac5f
EluciusFTW <guysbuss@gmail.com>
2023-02-18 11:21:30 +0100

    Moved column and row stuff to submodules

--- src/spectrecoff/Table.fs
+++ src/spectrecoff/Table.fs
@@ -5,90 +5,120 @@ open Spectre.Console
 open SpectreCoff.Layout
 open SpectreCoff.Output
 
+[<AutoOpen>]
+module Column = 
+    type ColumnLayout =
+        { Alignment: Alignment
+          LeftPadding: int
+          RightPadding: int
+          Wrap: bool }
+    
+    let defaultColumnLayout: ColumnLayout =
+        { Alignment = Center
+          LeftPadding = 2
+          RightPadding = 2
+          Wrap = true }
+
+    type ColumnDefinition = 
+        { Header: OutputPayload
+          Footer: Option<OutputPayload>
+          Layout: Option<ColumnLayout> }
+
+    let private applyDefiniteLayout (layout: ColumnLayout) (column: TableColumn) = 
+        match layout.Alignment with
+        | Left -> column.LeftAligned() |> ignore
+        | Right -> column.RightAligned() |> ignore
+        | Center -> column.Centered() |> ignore
+
+        column.PadLeft layout.LeftPadding |> ignore
+        column.PadRight layout.RightPadding |> ignore
+
+        column.NoWrap <- not layout.Wrap
+        column
+
+    let private applyLayout (possibleLayout: Option<ColumnLayout>) (column: TableColumn) =
+        match possibleLayout with 
+        | Some layout -> applyDefiniteLayout layout column
+        | None -> column
+
+    let private toSpectreContentColumn (payload: OutputPayload) =
+        TableColumn(payload |> payloadToRenderable)
+
+    let private addFooter footerOption (column: TableColumn) =
+        match footerOption with
+        | Some footer -> column.Footer <- payloadToRenderable footer
+        | None -> ignore()
+        column
+
+    let toSpectreColumn (definition: ColumnDefinition) =
+        toSpectreContentColumn definition.Header 
+        |> applyLayout definition.Layout
+        |> addFooter definition.Footer 
+    
+    let column header =
+        { Header = header; Footer = None; Layout = Some defaultColumnLayout }
+
+    let withLayout layout column = 
+        { column with Layout = Some layout }
+
+    let withLayouts layouts columns = 
+        columns
+        |> List.zip layouts 
+        |> List.map (fun (column, layout) -> { column with Layout = Some layout })
+
+    let withSameLayout layout columns =
+        columns |> List.map (fun column -> { column with Layout = Some layout })
+
+    let withFooter footer column = 
+        { column with Footer = Some footer }
+
+    let withFooters footers columns = 
+        columns
+        |> List.zip footers 
+        |> List.map (fun (column, footer) -> { column with Footer = Some footer })
+
+[<AutoOpen>]
+module Row = 
+    type Row =
+        | Payloads of OutputPayload list
+        | Strings of string list
+        | Numbers of int list
+
+    let private getValues (row: Row) =
+        match row with
+        | Payloads payloads -> payloads |> List.map payloadToRenderable
+        | Strings values -> values |> List.map (fun value -> Text value)
+        | Numbers values -> values |> List.map (fun value -> Text (value.ToString()))
+        |> List.toArray
+
+    let addRowToTable (table: Table) (row: Row) =
+        let values = getValues row
+        table.AddRow(values) |> ignore
+
+    let addRowToGrid (grid: Grid) (row: Row) =
+        let values = getValues row
+        grid.AddRow(values) |> ignore
+
 type TableLayout =
     {  Border: TableBorder;
        Sizing: SizingBehaviour;
        HideHeaders: bool;
+       HideFooters: bool;
        Alignment: Alignment }
-
-type ColumnLayout =
-    {  Alignment: Alignment
-       LeftPadding: int
-       RightPadding: int
-       Wrap: bool }
 
 let defaultTableLayout: TableLayout =
     {  Border = TableBorder.Rounded
        Sizing = Expand
        Alignment = Left
-       HideHeaders = false }
-
-let defaultColumnLayout: ColumnLayout =
-    {  Alignment = Center
-       LeftPadding = 2
-       RightPadding = 2
-       Wrap = true }
-
-type Row =
-    | Payloads of OutputPayload list
-    | Strings of string list
-    | Numbers of int list
-
-type ColumnDef = 
-    { H: OutputPayload
-      F: Option<OutputPayload>
-      L: Option<ColumnLayout> }
-
-let private applyDefiniteLayout (layout: ColumnLayout) (column: TableColumn) = 
-    match layout.Alignment with
-    | Left -> column.LeftAligned() |> ignore
-    | Right -> column.RightAligned() |> ignore
-    | Center -> column.Centered() |> ignore
-
-    column.PadLeft layout.LeftPadding |> ignore
-    column.PadRight layout.RightPadding |> ignore
-
-    column.NoWrap <- not layout.Wrap
-    column
-
-let private applyLayout (possibleLayout: Option<ColumnLayout>) (column: TableColumn) =
-    match possibleLayout with 
-    | Some layout -> applyDefiniteLayout layout column
-    | None -> column
-
-let private toSpectreContentColumn (payload: OutputPayload) =
-    TableColumn(payload |> payloadToRenderable)
-
-let private toSpectreColumn (definition: ColumnDef) =
-    toSpectreContentColumn definition.H |> applyLayout definition.L
-
-let private getValues (row: Row) =
-    match row with
-    | Payloads payloads -> payloads |> List.map payloadToRenderable
-    | Strings values -> values |> List.map (fun value -> Text value)
-    | Numbers values -> values |> List.map (fun value -> Text (value.ToString()))
-    |> List.toArray
-
-let private addRowToTable (table: Table) (row: Row) =
-    let values = getValues row
-    table.AddRow(values) |> ignore
-
-let private addRowToGrid (grid: Grid) (row: Row) =
-    let values = getValues row
-    grid.AddRow(values) |> ignore
-
-let toLaidOutColumn layout header =
-     { H = header; F = None; L = Some layout }
-
-let toColumnDefinitions headers = 
-    headers |> List.map (toLaidOutColumn defaultColumnLayout)
+       HideHeaders = false 
+       HideFooters = false }
 
 let toOutputPayload table =
     table
     :> Rendering.IRenderable
     |> Renderable
 
-let customTable (layout: TableLayout) (columnDefinitions: ColumnDef list) (rows: Row list) =
+let customTable (layout: TableLayout) (columnDefinitions: ColumnDefinition list) (rows: Row list) =
     let table = Table()
 
     match layout.Alignment with
@@ -101,9 +131,8 @@ let customTable (layout: TableLayout) (columnDefinitions: ColumnDef list) (rows:
     | Collapse -> table.Collapse() |> ignore
     table.Border <- layout.Border
 
-    match layout.HideHeaders with
-    | true -> table.HideHeaders() |> ignore
-    | false -> ()
+    table.ShowHeaders <- not layout.HideHeaders
+    table.ShowFooters <- not layout.HideFooters
 
     columnDefinitions 
     |> List.map toSpectreColumn
@@ -111,6 +140,7 @@ let customTable (layout: TableLayout) (columnDefinitions: ColumnDef list) (rows:
  
     rows |> List.iter (addRowToTable table)
     table
+
 
 let table =
     customTable defaultTableLayout
@@ -127,9 +157,7 @@ let grid (rows: Row list) =
 
     let grid = Grid().AddColumns numberOfColumns
     rows
-    |> List.map getValues
-    |> List.map grid.AddRow
-    |> ignore
+    |> List.iter (addRowToGrid grid)
     grid
 
 type Table with
