5d6c80c8c51acea327c28c326b4e5093a262b54a
EluciusFTW <guysbuss@gmail.com>
2023-05-18 12:17:34 +0200

    Use look in json

--- src/spectrecoff/Json.fs
+++ src/spectrecoff/Json.fs
@@ -5,46 +5,56 @@ open Spectre.Console
 open Spectre.Console.Json
 open SpectreCoff.Output
 
-let mutable bracesColor = calmLook.Color
-let mutable bracesDecorations = [ Decoration.None ]
+let mutable bracesLook = 
+    { Color = calmLook.Color
+      Decoration = [ Decoration.None ] }
 
-let mutable bracketsColor = calmLook.Color
-let mutable bracketsDecorations = [ Decoration.None ]
+let mutable bracketsLook = 
+    { Color = calmLook.Color
+      Decoration = [ Decoration.None ] }
 
-let mutable colonColor = calmLook.Color
-let mutable colonDecorations = [ Decoration.None ]
+let mutable colonLook = 
+    { Color = calmLook.Color
+      Decoration = [ Decoration.None ] }
 
-let mutable commaColor = calmLook.Color
-let mutable commaDecorations = [ Decoration.None ]
+let mutable commaLook = 
+    { Color = calmLook.Color
+      Decoration = [ Decoration.None ] }
 
-let mutable memberColor = pumpedLook.Color
-let mutable memberDecorations = [ Decoration.Italic ]
+let mutable memberLook = 
+    { Color = pumpedLook.Color
+      Decoration = [ Decoration.Italic ] }
 
-let mutable stringColor = edgyLook.Color
-let mutable stringDecorations = [ Decoration.Bold ]
+let mutable stringLook = 
+    { Color = edgyLook.Color
+      Decoration = [ Decoration.Bold ] }
 
-let mutable numberColor = edgyLook.Color
-let mutable numberDecorations = [ Decoration.Bold ]
+let mutable numberLook = 
+    { Color = edgyLook.Color
+      Decoration = [ Decoration.Bold ] }
 
-let mutable booleanColor = calmLook.Color
-let mutable booleanDecorations = [ Decoration.Bold ]
+let mutable booleanLook = 
+    { Color = calmLook.Color
+      Decoration = [ Decoration.Bold ] }
 
-let mutable nullColor = calmLook.Color
-let mutable nullDecorations = [ Decoration.Dim ]
+let mutable nullLook = 
+    { Color = calmLook.Color
+      Decoration = [ Decoration.Dim ] }
+
 
 let private aggregate decorations =
     decorations |>  List.reduce (|||)
 
 let private applyStyles (json: JsonText) =
-    json.BracesStyle <- Style (bracesColor, System.Nullable(), aggregate bracesDecorations)
-    json.BracketsStyle <- Style (bracketsColor, System.Nullable(), aggregate bracesDecorations)
-    json.ColonStyle <- Style (colonColor, System.Nullable(), aggregate colonDecorations)
-    json.CommaStyle <- Style (commaColor, System.Nullable(), aggregate commaDecorations)
-    json.StringStyle <- Style (stringColor, System.Nullable(), aggregate stringDecorations)
-    json.NumberStyle <- Style (numberColor, System.Nullable(), aggregate numberDecorations)
-    json.BooleanStyle <- Style (booleanColor, System.Nullable(), aggregate booleanDecorations)
-    json.MemberStyle <- Style (memberColor, System.Nullable(), aggregate memberDecorations)
-    json.NullStyle <- Style (nullColor, System.Nullable(), aggregate nullDecorations)
+    json.BracesStyle <- toStyle bracesLook
+    json.BracketsStyle <- toStyle bracketsLook
+    json.ColonStyle <- toStyle colonLook
+    json.CommaStyle <- toStyle commaLook
+    json.StringStyle <- toStyle stringLook
+    json.NumberStyle <- toStyle numberLook
+    json.BooleanStyle <- toStyle booleanLook
+    json.MemberStyle <- toStyle memberLook
+    json.NullStyle <- toStyle nullLook
     json
 
 let json content = 
