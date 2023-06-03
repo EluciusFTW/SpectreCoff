8104efe6b8db74bfe2a6537754c1515015d4b2bb
EluciusFTW <guysbuss@gmail.com>
2023-06-03 12:48:47 +0200

    Adjust figlet module to optional color

--- src/spectrecoff/Figlet.fs
+++ src/spectrecoff/Figlet.fs
@@ -8,18 +8,31 @@ open SpectreCoff.Output
 let mutable defaultAlignment = Center
 let mutable defaultColor = pumpedLook.Color
 
-let customFiglet (alignment: Alignment) (color: Color) content = 
-    let figlet = FigletText content
-    figlet.Color <- color
-
+let private applyAlignment alignment figlet = 
     match alignment with
     | Left -> figlet.LeftJustified() |> ignore
     | Center -> figlet.Centered() |> ignore
     | Right -> figlet.RightJustified() |> ignore
+    figlet
 
+let private toRenderable figlet = 
     figlet
     :> Rendering.IRenderable
     |> Renderable
 
-let figlet = 
-    customFiglet defaultAlignment defaultColor 
+let private applyColor (colorOption: Color Option) (figlet: FigletText) = 
+    match colorOption with 
+    | Some color ->  figlet.Color <- color
+    | None -> ()
+    figlet
+
+let customFiglet (alignment: Alignment) (color: Color) content = 
+    FigletText content
+    |> applyColor (Some color)
+    |> applyAlignment alignment
+    |> toRenderable
+
+let figlet content = 
+    FigletText content
+    |> applyAlignment defaultAlignment
+    |> toRenderable 
