f5f3949499a0be7350f9076f51355053dc10ac14
EluciusFTW <guysbuss@gmail.com>
2023-06-04 10:30:17 +0200

    Fixed color mapping

--- src/spectrecoff/Layout.fs
+++ src/spectrecoff/Layout.fs
@@ -25,15 +25,10 @@ type Look =
 let private aggregate decorations =
     decorations |>  List.reduce (|||)
 
-let toSpectreStyle look =
-    let backgroundColor = 
-        match look.BackgroundColor with
-        | Some color -> System.Nullable<Color> color
-        | None -> System.Nullable()
-    
-    let foregroundColor = 
-        match look.BackgroundColor with
-        | Some color -> System.Nullable<Color> color
-        | None -> System.Nullable()
+let private toNullable colorOption = 
+    match colorOption with 
+    | Some color -> System.Nullable<Color> color
+    | None -> System.Nullable()
 
-    Style (foregroundColor, backgroundColor, aggregate look.Decorations)
+let toSpectreStyle look =
+    Style (toNullable look.Color, toNullable look.BackgroundColor, aggregate look.Decorations)
