05fd9816d5eca85d3630dde13d6cf6e9a45cca80
EluciusFTW <guysbuss@gmail.com>
2023-07-08 14:27:49 +0200

    Fixed warnings and errors

--- src/spectrecoff-cli/Program.fs
+++ src/spectrecoff-cli/Program.fs
@@ -6,174 +6,234 @@ let main argv =
 
     let app = CommandApp()
     app.Configure(fun config ->
-        config.AddBranch("output", fun(add: IConfigurator<OutputSettings>) ->
-            add.AddCommand<OutputExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of printing things to the console.")
-                |> ignore
+        config
+            .AddBranch("output", fun(branchConfig: IConfigurator<OutputSettings>) ->
+                branchConfig
+                    .AddCommand<OutputExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of printing things to the console.")
+                    |> ignore
 
-            add.AddCommand<OutputDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation of the output module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<OutputDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation of the output module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("table", fun(add: IConfigurator<TableSettings>) ->
-            add.AddCommand<TableExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of tables.")
-                |> ignore
+        config
+            .AddBranch("table", fun(branchConfig: IConfigurator<TableSettings>) ->
+                branchConfig
+                    .AddCommand<TableExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of tables.")
+                    |> ignore
 
-            add.AddCommand<TableDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation of the table module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<TableDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation of the table module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("rule", fun(add: IConfigurator<RuleSettings>) ->
-            add.AddCommand<RuleExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples for rendering rules.")
-                |> ignore
+        config
+            .AddBranch("rule", fun(branchConfig: IConfigurator<RuleSettings>) ->
+                branchConfig
+                    .AddCommand<RuleExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples for rendering rules.")
+                    |> ignore
 
-            add.AddCommand<RuleDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the rule module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<RuleDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the rule module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("figlet", fun(add: IConfigurator<FigletSettings>) ->
-            add.AddCommand<FigletExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples for rendering figlets.")
-                |> ignore
+        config
+            .AddBranch("figlet", fun(branchConfig: IConfigurator<FigletSettings>) ->
+                branchConfig
+                    .AddCommand<FigletExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples for rendering figlets.")
+                    |> ignore
 
-            add.AddCommand<FigletDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the figlet module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<FigletDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the figlet module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("prompt", fun(add: IConfigurator<PromptSettings>) ->
-            add.AddCommand<PromptExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of prompts.")
-                |> ignore
+        config
+            .AddBranch("prompt", fun(branchConfig: IConfigurator<PromptSettings>) ->
+                branchConfig
+                    .AddCommand<PromptExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of prompts.")
+                    |> ignore
 
-            add.AddCommand<PromptDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the prompt module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<PromptDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the prompt module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("panel", fun(add: IConfigurator<PanelSettings>) ->
-            add.AddCommand<PanelExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of panels.")
-                |> ignore
+        config
+            .AddBranch("panel", fun(branchConfig: IConfigurator<PanelSettings>) ->
+                branchConfig
+                    .AddCommand<PanelExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of panels.")
+                    |> ignore
 
-            add.AddCommand<PanelDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the panel module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<PanelDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the panel module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("bar", fun(add: IConfigurator<BarChartSettings>) ->
-            add.AddCommand<BarChartExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of bar charts.")
-                |> ignore
+        config
+            .AddBranch("bar", fun(branchConfig: IConfigurator<BarChartSettings>) ->
+                branchConfig
+                    .AddCommand<BarChartExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of bar charts.")
+                    |> ignore
 
-            add.AddCommand<BarChartDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the bar charts module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<BarChartDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the bar charts module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("breakdown", fun(add: IConfigurator<BreakdownChartSettings>) ->
-            add.AddCommand<BreakdownChartExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of breakdown charts.")
-                |> ignore
+        config
+            .AddBranch("breakdown", fun(branchConfig: IConfigurator<BreakdownChartSettings>) ->
+                branchConfig
+                    .AddCommand<BreakdownChartExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of breakdown charts.")
+                    |> ignore
 
-            add.AddCommand<BreakdownChartDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the breakdown chart module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<BreakdownChartDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the breakdown chart module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("tree", fun(add: IConfigurator<TreeSettings>) ->
-            add.AddCommand<TreeExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of trees.")
-                |> ignore
+        config
+            .AddBranch("tree", fun(branchConfig: IConfigurator<TreeSettings>) ->
+                branchConfig
+                    .AddCommand<TreeExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of trees.")
+                    |> ignore
 
-            add.AddCommand<TreeDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the tree module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<TreeDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the tree module.")
+                    |> ignore
+            )|> ignore
 
-        config.AddBranch("calendar", fun(add: IConfigurator<CalendarSettings>) ->
-            add.AddCommand<CalendarExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of calendars.")
-                |> ignore
+        config
+            .AddBranch("calendar", fun(branchConfig: IConfigurator<CalendarSettings>) ->
+                branchConfig
+                    .AddCommand<CalendarExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of calendars.")
+                    |> ignore
 
-            add.AddCommand<CalendarDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the calendar module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<CalendarDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the calendar module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("padder", fun(add: IConfigurator<PadderSettings>) ->
-            add.AddCommand<PadderExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of padders.")
-                |> ignore
+        config
+            .AddBranch("padder", fun(branchConfig: IConfigurator<PadderSettings>) ->
+                branchConfig
+                    .AddCommand<PadderExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of padders.")
+                    |> ignore
 
-            add.AddCommand<PadderDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the padder module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<PadderDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the padder module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("grid", fun(add: IConfigurator<GridSettings>) ->
-            add.AddCommand<GridExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of grids.")
-                |> ignore
+        config
+            .AddBranch("grid", fun(branchConfig: IConfigurator<GridSettings>) ->
+                branchConfig
+                    .AddCommand<GridExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of grids.")
+                    |> ignore
 
-            add.AddCommand<GridDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the grid module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<GridDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the grid module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("textpath", fun(add: IConfigurator<TextpathSettings>) ->
-            add.AddCommand<TextpathExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of text paths.")
-                |> ignore
+        config
+            .AddBranch("textpath", fun(branchConfig: IConfigurator<TextpathSettings>) ->
+                branchConfig
+                    .AddCommand<TextpathExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of text paths.")
+                    |> ignore
 
-            add.AddCommand<TextpathDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the text path module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<TextpathDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the text path module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("json", fun(add: IConfigurator<JsonSettings>) ->
-            add.AddCommand<JsonExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of json output.")
-                |> ignore
+        config
+            .AddBranch("json", fun(branchConfig: IConfigurator<JsonSettings>) ->
+                branchConfig
+                    .AddCommand<JsonExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of json output.")
+                    |> ignore
 
-            add.AddCommand<JsonDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the json module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<JsonDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the json module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddBranch("canvasimage", fun(add: IConfigurator<CanvasImageSettings>) ->
-            add.AddCommand<CanvasImageExample>("example")
-                .WithAlias("e")
-                .WithDescription("Shows examples of canvas image output.")
-                |> ignore
+        config
+            .AddBranch("canvasimage", fun(branchConfig: IConfigurator<CanvasImageSettings>) ->
+                branchConfig
+                    .AddCommand<CanvasImageExample>("example")
+                    .WithAlias("e")
+                    .WithDescription("Shows examples of canvas image output.")
+                    |> ignore
 
-            add.AddCommand<CanvasImageDocumentation>("doc")
-                .WithAlias("d")
-                .WithDescription("Shows the documentation for the canvas image module.")
-                |> ignore)
+                branchConfig
+                    .AddCommand<CanvasImageDocumentation>("doc")
+                    .WithAlias("d")
+                    .WithDescription("Shows the documentation for the canvas image module.")
+                    |> ignore
+            ) |> ignore
 
-        config.AddCommand<Progress>("progress")
+        config
+            .AddCommand<Progress>("progress")
             .WithDescription("Shows which modules from Spectre.Console have been ported to SpectreCoff.")
-            |> ignore
-        )
+            |> ignore)
 
     app.Run(argv)
