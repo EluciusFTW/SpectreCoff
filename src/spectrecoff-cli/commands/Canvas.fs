47e8b7803d8c82c130e9ad343db59ee51f0eabce
EluciusFTW <guysbuss@gmail.com>
2023-07-28 16:47:49 +0200

    Added command boilerplate

---
+++ src/spectrecoff-cli/commands/Canvas.fs
@@ -0,0 +1,28 @@
+﻿namespace SpectreCoff.Cli.Commands
+
+open Spectre.Console
+open Spectre.Console.Cli
+open SpectreCoff
+
+type CanvasSettings() =
+    inherit CommandSettings()
+
+type CanvasExample() =
+    inherit Command<CanvasSettings>()
+    interface ICommandLimiter<CanvasSettings>
+
+    override _.Execute(_context, _settings) =
+        0
+
+type CanvasDocumentation() =
+    inherit Command<CanvasSettings>()
+    interface ICommandLimiter<CanvasSettings>
+
+    override _.Execute(_context, _settings) =
+        Cli.Theme.setDocumentationStyle
+        EL |> toConsole
+        
+        pumped "Canvas module"
+        |> alignedRule Left
+        |> toConsole
+        0
