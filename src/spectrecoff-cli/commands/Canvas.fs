b2d4f16cc651d3005e7782e17d022c05bfb3c8d1
EluciusFTW <guysbuss@gmail.com>
2023-07-29 19:02:18 +0200

    Canvas example

--- src/spectrecoff-cli/commands/Canvas.fs
+++ src/spectrecoff-cli/commands/Canvas.fs
@@ -12,6 +12,17 @@ type CanvasExample() =
     interface ICommandLimiter<CanvasSettings>
 
     override _.Execute(_context, _settings) =
+        
+        canvas (Width 12) (Height 12) 
+        |> withPixels (Rectangle (0,0,11,11)) Color.Yellow
+        |> withPixels (Rectangle (2,2,4,3)) Color.Purple
+        |> withPixels (Rectangle (7,2,9,3)) Color.Purple
+        |> withPixels (ColumnSegment (8,4,7)) Color.Blue
+        |> withPixels (RowSegment (9,3,8)) Color.Purple
+        |> withPixels (Canvas.Many [(3,10); (8,10)]) Color.Purple
+        |> toOutputPayload 
+        |> toConsole
+
         0
 
 type CanvasDocumentation() =
