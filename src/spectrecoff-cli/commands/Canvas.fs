69d8a99d1f119e0afeb6f62a7cafa8b7ea1b6513
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-08-04 08:57:37 +0200

    Replace forgotten newlines

--- src/spectrecoff-cli/commands/Canvas.fs
+++ src/spectrecoff-cli/commands/Canvas.fs
@@ -12,15 +12,15 @@ type CanvasExample() =
     interface ICommandLimiter<CanvasSettings>
 
     override _.Execute(_context, _settings) =
-        
-        canvas (Width 12) (Height 12) 
+
+        canvas (Width 12) (Height 12)
         |> withPixels (Rectangle (0,0,11,11)) Color.Yellow
         |> withPixels (Rectangle (2,2,4,3)) Color.Purple
         |> withPixels (Rectangle (7,2,9,3)) Color.Purple
         |> withPixels (ColumnSegment (ColumnIndex 8, StartIndex 4, EndIndex 7)) Color.Blue
         |> withPixels (RowSegment (RowIndex 9, StartIndex 3, EndIndex 8)) Color.Purple
         |> withPixels (MultiplePixels [(3,10); (8,10)]) Color.Purple
-        |> toOutputPayload 
+        |> toOutputPayload
         |> toConsole
 
         0
@@ -31,19 +31,19 @@ type CanvasDocumentation() =
 
     override _.Execute(_context, _settings) =
         Cli.Theme.setDocumentationStyle
-        EL |> toConsole
-        
+        BL |> toConsole
+
         pumped "Canvas module"
         |> alignedRule Left
         |> toConsole
-        
+
         Many [
             Many [
                 C "This module provides functionality from the canvas widget of Spectre.Console ("
                 Link "https://spectreconsole.net/widgets/canvas"
                 C ")"
             ]
-            EL
+            BL
             Edgy "Sorry, this documentation is not available yet."
         ] |> toConsole
         0
