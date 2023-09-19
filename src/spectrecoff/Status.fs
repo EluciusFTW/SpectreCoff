ca31f88a8788d67523d4d8b1bd8ed27fd48f19f3
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-09-19 11:49:54 +0200

    Rename and return context

--- src/spectrecoff/Status.fs
+++ src/spectrecoff/Status.fs
@@ -12,38 +12,38 @@ type CustomSpinner =
 let start statusText (operation: StatusContext -> Task<unit>) =
     task { return! AnsiConsole.Status().StartAsync(statusText, operation) }
 
-let startWithCustomSpinner customerSpinner (operation: StatusContext -> Task<unit>) =
+let startWithCustomSpinner spinner (operation: StatusContext -> Task<unit>) =
     task {
         let status = AnsiConsole.Status()
 
         status.Spinner <-
-            match customerSpinner.Spinner with
+            match spinner.Spinner with
             | Some spinner -> spinner
             | None _ -> status.Spinner
 
         status.SpinnerStyle <-
-            match customerSpinner.Look with
+            match spinner.Look with
             | Some look -> look |> toSpectreStyle
             | None _ -> status.SpinnerStyle
 
-        return! status.StartAsync(customerSpinner.Message, operation)
+        return! status.StartAsync(spinner.Message, operation)
     }
 
 let update newMessage (context: StatusContext) =
     context.Status <- newMessage
     context
 
-let updateWithCustomSpinner customerSpinner (context: StatusContext) =
-    context.Status <- customerSpinner.Message
+let updateWithCustomSpinner spinner (context: StatusContext) =
+    context.Status <- spinner.Message
 
     context.Spinner <-
-        match customerSpinner.Spinner with
+        match spinner.Spinner with
         | Some spinner -> spinner
         | None _ -> context.Spinner
 
     context.SpinnerStyle <-
-        match customerSpinner.Look with
+        match spinner.Look with
         | Some look -> look |> toSpectreStyle
         | None _ -> context.SpinnerStyle
 
-    context.Spinner
+    context
