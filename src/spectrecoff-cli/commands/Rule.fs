04b93fa11231d2bfc390579ab9c0eb9f48ad4f0a
EluciusFTW <guysbuss@gmail.com>
2023-11-28 08:52:56 +0100

    Migrated rule doc

--- src/spectrecoff-cli/commands/Rule.fs
+++ src/spectrecoff-cli/commands/Rule.fs
@@ -37,17 +37,14 @@ type RuleDocumentation() =
         Many [
             docSynopsis "Rule module" "This module provides functionality from the rule widget of Spectre.Console" "widgets/rule"
             BL
-            C "The rule can be used by the rule function:"
-            BI [
-                P "rule: string -> OutputPayload"
+            C "Rules can be created using one of these functions:"
+            funcsOutput [
+                { Name = "rule"; Signature = "string -> OutputPayload" }
+                { Name = "alignedRule"; Signature = "Alignment -> string -> OutputPayload" }
             ]
             BL
-            C "This rule will use the"; P "Rule.defaultAlignment,"; C "which is set to"; P "Center"; C "but can be modified."
-            BL
-            C "Other rules can be used without changing the default by passing in the alignment as an argument to: "
-            BI [
-                P "alignedRule: Alignment -> string -> OutputPayload"
-            ]
+            C "The former will use the"; P "Rule.defaultAlignment,"; C "which is set to"; P "Center"; C "but can be modified."
+            C "Without changing the default, the latter function also accepts an alignment as an argument."
             BL
             C "The rule can be printed to the console with the"; P "toConsole"; C "function."
             BL
