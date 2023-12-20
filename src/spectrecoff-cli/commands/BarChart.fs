6309a3f87a4bdbc917393ca9d7869dee73f8d1b5
EluciusFTW <guysbuss@gmail.com>
2023-12-20 11:07:42 +0100

    Migrated barchart docs

--- src/spectrecoff-cli/commands/BarChart.fs
+++ src/spectrecoff-cli/commands/BarChart.fs
@@ -41,23 +41,23 @@ type BarChartDocumentation() =
             spectreDocSynopsis "BarChart submodule" "This module provides functionality from the bar chart widget of Spectre.Console" "widgets/barchart"
             BL
             C "The bar chart can be used using the barChart function:"
-            BI [
-                P "barChart: string -> ChartItem list -> OutputPayload"
+            funcsOutput [
+                { Name = "barChart"; Signature = "string -> ChartItem list -> OutputPayload" }
             ]
             BL
             C "The"; P "ChartItem"; C "union type consists of two options:"
-            BI [
-                Many [P "ChartItem:"; C "Consists of the label and a value for the item."]
-                Many [P "ChartItemWithColor:"; C "Additionally defines a color the item will be rendered in."]
+            discriminatedUnionOutput [
+                { Label = "ChartItem"; Args = ["string"; "float"]; Explanation = "Consists of the label and a value for the item" }
+                { Label = "ChartItemWithColor"; Args = ["string"; "float"; "Color"]; Explanation = "Consists of the label and a value for the item, as well as the color." }
             ]
             BL
             C "If no color is explicitly defined, the colors will cycle through a set of colors defined in the"; P "Colors"; C "variable."
             C "This variable can be overwritten with a custom set if the default one is not to your taste."
             BL
             C "Similarly, two other variables can be overwritten:"
-            BI [
-                Many [P "width:"; C "Controls the width of the whole chart."]
-                Many [P "alignment:"; C "Controls the alignment of the chart's label."]
+            valuesOutput [
+                { Name = "width"; Type = "Integer"; DefaultValue = "60"; Explanation = "Determines the width of the whole chart" }
+                { Name = "alignment"; Type = "Alignment"; DefaultValue = "Center"; Explanation = "Determines the alignment of the chart's label" }
             ]
         ] |> toConsole
         0
