5f7ba3a6fc7de06033940432bd311f32766ef221
EluciusFTW <guysbuss@gmail.com>
2024-01-04 16:26:36 +0100

    Updated canvas and added documentation

--- src/spectrecoff/Spectre/Canvas.fs
+++ src/spectrecoff/Spectre/Canvas.fs
@@ -9,6 +9,7 @@ module Width =
     let unwrap (Width w) = w
 
 type Height = Height of int
+
 [<RequireQualifiedAccess>]
 module Height =
     let unwrap (Height h) = h
@@ -17,28 +18,29 @@ type ColumnIndex = ColumnIndex of int
 type RowIndex = RowIndex of int
 type StartIndex = StartIndex of int
 type EndIndex = EndIndex of int
-type PixelPosition = int*int
+type Point = int*int
+
 type Pixels = 
-    | SinglePixel of PixelPosition
-    | MultiplePixels of PixelPosition list
+    | Pixel of Point
+    | Pixels of Point list
     | Row of RowIndex
     | RowSegment of RowIndex*StartIndex*EndIndex
     | Column of ColumnIndex
     | ColumnSegment of ColumnIndex*StartIndex*EndIndex
-    | Rectangle of int*int*int*int
+    | Rectangle of Point*Point
 
 let private setPixel (canvas: Canvas) color pixel = 
     canvas.SetPixel(fst pixel, snd pixel, color) |> ignore
 
 let rec private getPixelPositions (canvas: Canvas) pixels = 
     match pixels with
-    | SinglePixel p -> [p]
-    | MultiplePixels ps -> ps
+    | Pixel p -> [p]
+    | Pixels ps -> ps
     | RowSegment (RowIndex r, StartIndex s, EndIndex e) -> [ for x in s .. e -> (x, r)] 
     | ColumnSegment (ColumnIndex c, StartIndex s, EndIndex e) -> [ for y in s .. e -> (c, y)]
     | Row row -> getPixelPositions canvas (RowSegment (row, StartIndex 0, EndIndex canvas.Width))
     | Column column -> getPixelPositions canvas (ColumnSegment (column, StartIndex 0, EndIndex canvas.Height))
-    | Rectangle (x1,y1,x2,y2) -> 
+    | Rectangle ((x1, y1), (x2, y2)) -> 
         [ for col in x1 .. x2 -> ColumnSegment(ColumnIndex col, StartIndex y1, EndIndex y2) ] 
         |> List.map (getPixelPositions canvas)
         |> List.collect id
